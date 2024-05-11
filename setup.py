b3acc1e9bdb2b0bb52fc7b1c6640a28b640591eb
rahuldalvi0007 <rahuldalvi14441444@gmail.com>
2024-05-11 23:35:44 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='rahul',
+author_email='rahuldalvi14441444@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn'],
+)
+
+
+
+
